914c5a48e734f70f860f5c8fb14ffb83e6b95d0c
Jeremie Corkery <jeremiecorkery14@gmail.com>
2021-04-27 17:08:32 -0400

    Added trapezoid and simpsons integration rules

---
+++ pysics/functions.py
@@ -0,0 +1,42 @@
+def trapezoid(f, a, b, N):
+    """
+    Implements the trapezoid integration scheme. 
+    Arguments:
+        f: function that returns an integer value
+        a: left integration bound
+        b: right integration bound
+        N: number of partitions to divide the interval into (i.e. how many slices are we seperating our interval into)
+
+    trapezoid: function, float, float, int -> float
+
+    """
+
+    
+    h = (b-a)/N
+    sum = (f(a)+f(b))/2
+    for i in range(1,N):
+        sum += f(a+k*h)
+    
+    return sum
+
+def simpsons(f, a, b, N):
+    """
+    Implements Simpson's  integration scheme. 
+    Arguments:
+        f: function that returns an integer value
+        a: left integration bound
+        b: right integration bound
+        N: number of partitions to divide the interval into (i.e. how many slices are we seperating our interval into)
+
+    simpsons: function, float, float, int -> float
+
+    """    
+
+    h = (b-a)/N
+    sum = (f(a)+f(b))*(h/3)
+    for i in range(1,N):
+        sum += 4 * f(a+k*h)
+        sum += 2 * f(a+k*h)
+
+    return sum
+
